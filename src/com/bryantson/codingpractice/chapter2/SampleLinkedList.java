664db093f0af42f9894c67891f950db8696d7f3f
Bryant Son <lovepool@utexas.edu>
2015-08-22 16:50:24 -0500

    Added sample warmup practice for Chapter 2 exercise

---
+++ src/com/bryantson/codingpractice/chapter2/SampleLinkedList.java
@@ -0,0 +1,89 @@
+/**
+ * Warm up Practice for Chapter 2:
+ * 	Defines the LinkedList structure with append node function and remove node function
+ * 	
+ *
+ * 	@author Bryant Son
+ * 	@since  08/22/2015
+ */
+package com.bryantson.codingpractice.chapter2;
+
+public class SampleLinkedList {
+	private Node head;
+        private int  size;
+
+	public SampleLinkedList(int data) {
+		this.head = new Node(data);
+		this.size = 1;
+	}
+
+	public Node getHead() {
+		return this.head;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+
+	public void print() {
+		
+		Node curr = this.head;
+
+		while (curr != null) {
+			System.out.print(curr.data);
+			
+			System.out.print(" => "); 
+			
+			curr = curr.next;
+		}
+
+		System.out.print(" null");
+		System.out.println();
+
+	}
+
+	public Node append(int data) {
+		Node curr = this.head;
+
+		while (curr.next != null) {
+			curr = curr.next;
+		}
+		
+		curr.next = new Node(data);	
+
+		++ this.size;
+		return head;
+	}	
+
+	public Node removeNode(int data) {
+		if (data == this.head.data) {
+			this.head = this.head.next;
+			return this.head;
+		}
+		
+		Node curr = this.head;
+
+		while(curr.next != null) {
+			if(curr.next.data == data) {
+				curr.next = curr.next.next;
+				-- this.size;
+				return this.head;
+			}
+			
+			curr = curr.next;
+
+		}
+		return this.head;
+	}
+
+}
+
+class Node {
+	public int data;
+	public Node next;
+
+	public Node(int data){ 
+		this.data = data;
+		this.next = null;
+	}
+}
---
+++ test/com/bryantson/codingpractice/chapter1/Problem2_SampleLinkedListTest.java
@@ -0,0 +1,50 @@
+/**
+ * Problem2_SampleLinkedListTest
+ * 		Tests and verifies the different cases for SampleLinkedList
+ * 
+ * @author Bryant J. Son
+ * @since  08/22/2015
+ */
+
+
+package com.bryantson.codingpractice.chapter1;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import com.bryantson.codingpractice.chapter2.SampleLinkedList;
+
+public class Problem2_SampleLinkedListTest {
+	private SampleLinkedList sampleLinkedList;
+	private int expected;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.sampleLinkedList = new SampleLinkedList(1);
+		this.sampleLinkedList.append(2);
+		this.sampleLinkedList.append(3);
+	}
+
+	@Test
+	public void testAppend_ReturnsSize5() {
+		expected = 5;
+		this.sampleLinkedList.append(4);
+		this.sampleLinkedList.append(5);
+		assertEquals(5, this.sampleLinkedList.size());
+	}
+	
+	@Test
+	public void testRemove_ReturnsSize2() {
+		expected = 2;
+		this.sampleLinkedList.removeNode(2);
+		assertEquals(expected, this.sampleLinkedList.size());
+	}
+	
+	@Test
+	public void testRemoveNotExistingElement_ReturnsSize3() {
+		expected = 3;
+		this.sampleLinkedList.removeNode(4);
+		assertEquals(expected, this.sampleLinkedList.size());
+	}
+
+}
